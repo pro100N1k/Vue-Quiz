61cea381a92828ab596d53cdf8681ebb5b421cd0
tkachyk.o <tkachyk.o@hexide-digital.com>
2020-02-07 16:37:55 +0200

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <Header
+        :numCorrect="numCorrect"
+        :numTotal="numTotal"
+        :numCurrent = "numCurrent"
+    />
+      <b-container>
+          <b-row>
+              <b-col sm="6" offset="3">
+                  <QuestionBox
+                     v-if="questions.length"
+                    :currentQuestion="questions[index]"
+                    :next="next"
+                     :increment="increment"
+                  />
+              </b-col>
+          </b-row>
+      </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import QuestionBox from "./components/QuestionBox";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Header,
+    QuestionBox
+  },
+    data() {
+      return {
+        questions: [],
+        index: 0,
+        numCurrent: 0,
+        numCorrect: 0,
+        numTotal: 0
+      }
+    },
+    methods: {
+      next() {
+          this.index++
+      },
+        increment(isCorrect) {
+          if(isCorrect) {
+              this.numCorrect++
+          }
+          this.numCurrent++
+        }
+    },
+    mounted: function() {
+      fetch('https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple', {
+          method: 'get'
+      })
+        .then((response) => {
+            return response.json();
+        })
+        .then((jsonData) => {
+            this.questions = jsonData.results
+            this.numTotal = this.questions.length
+        })
+    }
 }
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="">
+   <b-nav tabs>
+       <b-nav-item disabled active>Fancy Quiz App</b-nav-item>
+       <b-nav-item disabled>Counter: {{numCurrent}}/{{numTotal}} | Correct: {{numCorrect}}</b-nav-item>
+   </b-nav>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: [
+            'numCorrect',
+            'numCurrent',
+            'numTotal'
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
